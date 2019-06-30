05efaa6f31261b6f88d71a4b4925c51e753d7833
Robert Knop <robert.knop@nordakademie.org>
2019-06-30 23:27:55 +0200

    prepare for final submission

--- Main/3.6.3_AmazonReviews.py
+++ Main/3.6.3_AmazonReviews.py
@@ -20,11 +20,11 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import BernoulliNB
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import TruncatedSVD 
 from sklearn.utils import shuffle
 from sklearn.metrics import classification_report, confusion_matrix, roc_curve, auc
 from sklearn.model_selection import cross_val_score, GridSearchCV, train_test_split
-
-
 #%%
 import gzip
 
@@ -42,7 +42,9 @@ def getDF(path):
   return pd.DataFrame.from_dict(df, orient='index')
 
 # Read data
-df = getDF('Main/data/amazon-reviews/reviews_Home_and_Kitchen_5.json.gz')
+# filepath = 'Main/data/amazon-reviews/reviews_Home_and_Kitchen_5.json.gz'
+filepath = 'reviews_Home_and_Kitchen_5.json.gz'
+df = getDF(filepath)
 #%% [markdown]
 # ### Variable descriptions
 # * reviewerID - ID of the reviewer, e.g. A2SUAM1J3GNN3B
@@ -57,7 +59,7 @@ df = getDF('Main/data/amazon-reviews/reviews_Home_and_Kitchen_5.json.gz')
 
 #%%
 # Do first data profile report on raw data
-pp.ProfileReport(df, check_correlation=False, pool_size=15).to_file(outputfile="3.6.3_AmazonReviews_RAW.html")
+# pp.ProfileReport(df, check_correlation=False, pool_size=15).to_file(outputfile="3.6.3_AmazonReviews_RAW.html")
 # https://github.com/RobKnop/ThinkfulDataScienceBootcamp/blob/master/Main/3.6.3_AmazonReviews_RAW.html
 #%%
 # Drop unnecessary columns
@@ -76,7 +78,7 @@ df = df.drop(columns=['overall'])
 df = df.drop_duplicates() # Dataset has 70 duplicate rows
 #%%
 # Do second data profile report on cleaned data
-pp.ProfileReport(df, check_correlation=False, pool_size=15).to_file(outputfile="3.6.3_AmazonReviews_CLEAN.html")
+# pp.ProfileReport(df, check_correlation=False, pool_size=15).to_file(outputfile="3.6.3_AmazonReviews_CLEAN.html")
 # See the webpage at: https://github.com/RobKnop/ThinkfulDataScienceBootcamp/blob/master/Main/3.6.3_AmazonReviews_CLEAN_5mio.html
 #%%
 # Feature Engineering
@@ -95,7 +97,7 @@ df = df.drop(columns=[
 #   * DISTANCE - DISTANCE_GROUP - AIR_TIME
 # 3. Class imbalance: 17601697 - 2398303 (88%/12%)
 #%% [markdown]
-# #### Our key evaluation metric to optimize on is f1 score 
+# #### Our key evaluation metric to optimize on is accuracy, followed by the f1 score 
 # * A balance between precision and recall is needed. 
 #%% [markdown]
 # #### Models to try:
@@ -119,7 +121,6 @@ df_class_0_under = df_class_0.sample(count_class_1)
 df = pd.concat([df_class_0_under, df_class_1], axis=0)
 print(df.y_sentiment.value_counts())
 
-#%%
 # Define X and y
 X = TfidfVectorizer().fit_transform(df.corpus)
 y = df['y_sentiment']
@@ -128,7 +129,7 @@ y = df['y_sentiment']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
 #%%
 # Logistic Regression: 
-lr = LogisticRegression(penalty='l2', solver='lbfgs', max_iter=50, verbose=1, n_jobs=2)
+lr = LogisticRegression(penalty='l2', solver='lbfgs', max_iter=50, verbose=1, n_jobs=-1)
 
 # Fit the model.
 fit = lr.fit(X_train, y_train)
@@ -147,10 +148,10 @@ Under-Sampling:
            0       0.87      0.88      0.87     19242
            1       0.88      0.87      0.87     19348
 """
-score = cross_val_score(fit, X, y, cv=5, scoring='f1', n_jobs=2)
+score = cross_val_score(fit, X, y, cv=5, scoring='accuracy', n_jobs=-1)
 print('\f1: ', score)
-print("Cross Validated f1: %0.2f (+/- %0.2f)" % (score.mean(), score.std() * 2))
-# Cross Validated f1: 0.87 (+/- 0.00)
+print("Cross Validated f1: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
+# Cross Validated f1: 0.87 (+/- 0.01)
 #%%
 # Decision Tree:
 dt = tree.DecisionTreeClassifier()
@@ -162,16 +163,17 @@ parameters = {
               'min_samples_leaf': [1, 3, 5, 8]
              }
 # Run the grid search
-grid_obj = GridSearchCV(dt, parameters, scoring='recall', cv=3, n_jobs=2, verbose=1)
+grid_obj = GridSearchCV(dt, parameters, scoring='accuracy', cv=3, n_jobs=-1, verbose=1)
 grid_obj.fit(X, y)
 dt = grid_obj.best_estimator_
+dt
 #%%
 #Run best DT model:
 
-dt = tree.DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=10,
-            max_features=3, max_leaf_nodes=None, min_impurity_decrease=0.0,
-            min_impurity_split=None, min_samples_leaf=8,
-            min_samples_split=2, min_weight_fraction_leaf=0.0,
+dt = tree.DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=13,
+            max_features=1, max_leaf_nodes=None, min_impurity_decrease=0.0,
+            min_impurity_split=None, min_samples_leaf=5,
+            min_samples_split=3, min_weight_fraction_leaf=0.0,
             presort=False, random_state=None, splitter='best')
 
 # Fit the best algorithm to the data. 
@@ -191,7 +193,7 @@ Under-Sampling:
            0       0.50      1.00      0.67     19242
            1       0.00      0.00      0.00     19348
 """
-score = cross_val_score(dt, X, y, cv=10, scoring='f1', n_jobs=2, verbose=1)
+score = cross_val_score(dt, X, y, cv=10, scoring='accuracy', n_jobs=-1, verbose=1)
 print("DT: Input X --> f1: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
 # DT: Input X --> f1: 0.810 (+/- 0.029)
 #%%
@@ -214,12 +216,12 @@ Under-Sampling:
            0       0.75      0.58      0.65     19242
            1       0.66      0.81      0.72     19348
 """
-score = cross_val_score(bnb, X, y, cv=10, scoring='f1', n_jobs=2, verbose=1)
+score = cross_val_score(bnb, X, y, cv=10, scoring='accuracy', n_jobs=-1, verbose=1)
 print("BNB: Input X --> f1: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
 #BNB: Input X --> f1: 0.810 (+/- 0.005)
 #%%
 # Random Forest: 
-rfc = ensemble.RandomForestClassifier(criterion='entropy', n_jobs=2)
+rfc = ensemble.RandomForestClassifier(criterion='entropy', n_jobs=-1)
 
 # Choose some parameter combinations to try
 parameters = {'n_estimators': [16, 32, 64], 
@@ -231,25 +233,24 @@ parameters = {'n_estimators': [16, 32, 64],
              }
 
 # Run the grid search
-grid_obj = GridSearchCV(rfc, parameters, scoring='f1', cv=3, n_jobs=2, verbose=1)
+grid_obj = GridSearchCV(rfc, parameters, scoring='accuracy', cv=3, n_jobs=10, verbose=1)
 grid_obj.fit(X, y)
 
 # Set the clf to the best combination of parameters
 rfc = grid_obj.best_estimator_
-
+rfc
 #%%
 # Run best model:
 rfc = ensemble.RandomForestClassifier(bootstrap=True, class_weight=None, criterion='entropy',
             max_depth=13, max_features='auto', max_leaf_nodes=None,
             min_impurity_decrease=0.0, min_impurity_split=None,
-            min_samples_leaf=1, min_samples_split=5,
-            min_weight_fraction_leaf=0.0, n_estimators=32, n_jobs=2,
+            min_samples_leaf=5, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, n_estimators=64, n_jobs=-1,
             oob_score=False, random_state=None, verbose=0,
             warm_start=False)
 
 # Fit the best algorithm to the data. 
 rfc.fit(X_train, y_train)
-
 #%%
 y_pred = rfc.predict(X_test)
 print('Confusion Matrix\n', pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predicted'], margins=True))
@@ -261,51 +262,12 @@ Without Under-Sampling:
               precision    recall  f1-score   support
 
 Under-Sampling:
-           0       0.73      0.70      0.71     19242
-           1       0.71      0.74      0.72     19348
+           0       0.75      0.75      0.75     19242
+           1       0.75      0.75      0.75     19348
 '''
-score = cross_val_score(rfc, X, y, cv=10, scoring='f1', n_jobs=2, verbose=1)
+score = cross_val_score(rfc, X, y, cv=10, scoring='accuracy', n_jobs=-1, verbose=1)
 print("RFC: Input X --> f1: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
-# RFC: Input X --> f1: 0.738 (+/- 0.010)
-#%%
-# KNN:
-for k in range(6, 10, 1):
-    print('k = ', k)
-    neighbors = KNeighborsClassifier(n_neighbors=k, n_jobs=2, weights='distance')
-    neighbors.fit(X_train, y_train)
-    y_pred = neighbors.predict(X_test)
-    #print('Confusion Matrix\n', pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predicted'], margins=True))
-    print('KNN:\n', classification_report(y_test, y_pred, target_names=['0', '1']))
-    #fpr, tpr, thresholds = roc_curve(y_test, y_pred, pos_label=1)
-    #print('AUC: ', auc(fpr, tpr))
-    # Cross Validation
-    #score = cross_val_score(neighbors, X_test, y_test, cv=5, scoring='recall', n_jobs=2)
-    #print("KNN: Input X --> f1: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
-'''
-k=8
-               precision    recall  f1-score   support
-           0       0.96      0.98      0.97    176169
-           1       0.83      0.67      0.74     23831
-'''
-#%%
-# SVM:
-svc = SVC(gamma='scale', verbose=1)
-
-svc.fit(X_train, y_train)
-
-y_pred = svc.predict(X_test)
-print('Confusion Matrix\n', pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predicted'], margins=True))
-print('SVC:\n', classification_report(y_test, y_pred, target_names=['0', '1']))
-fpr, tpr, thresholds = roc_curve(y_test, y_pred, pos_label=1)
-print('AUC: ', auc(fpr, tpr))
-'''
-               precision    recall  f1-score   support
-           0       0.97      0.99      0.98     17642
-           1       0.90      0.76      0.82      2358
-'''
-score = cross_val_score(svc, X_train, y_train, cv=5, scoring='f1', n_jobs=2, verbose=1)
-print("Input X_train --> f1: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
-# Input X_train --> f1: 0.741 (+/- 0.014)
+# RFC: Input X --> f1: 0.739 (+/- 0.024)
 #%%
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=20)
 # Gradient Boosting
@@ -333,31 +295,72 @@ print('AUC: ', auc(fpr, tpr))
 Without Under-Sampling:
                precision    recall  f1-score   support
 Under-Sampling
-           0       0.95      0.95      0.95     24938
-           1       0.95      0.95      0.95     25041
+           0       0.83      0.85      0.84      9682
+           1       0.84      0.83      0.83      9613
 '''
-score = cross_val_score(gbc, X, y, cv=10, scoring='f1', n_jobs=2, verbose=1)
+score = cross_val_score(gbc, X, y, cv=10, scoring='accuracy', n_jobs=-1, verbose=1)
 print("GradBoost: Input X --> f1: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
 """
 GradBoost: Input X --> f1: 0.987 (+/- 0.004) - elapsed: 12.4min
 """
+#%%
+# Reduce dims
+sklearn_tSVD = TruncatedSVD(n_components=5)
+X_tSVD = sklearn_tSVD.fit_transform(X)
+# Split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X_tSVD, y, test_size=0.2, random_state=20)
+#%%
+scaling = MinMaxScaler(feature_range=(-1,1)).fit(X_train)
+X_train = scaling.transform(X_train)
+X_test = scaling.transform(X_test)
+# SVM:
+svc = SVC(gamma='scale', verbose=1)
+y_test
+svc.fit(X_train, y_train)
 
+y_pred = svc.predict(X_test)
+print('Confusion Matrix\n', pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predicted'], margins=True))
+print('SVC:\n', classification_report(y_test, y_pred, target_names=['0', '1']))
+fpr, tpr, thresholds = roc_curve(y_test, y_pred, pos_label=1)
+print('AUC: ', auc(fpr, tpr))
+'''
+               precision    recall  f1-score   support
+           0       0.69      0.82      0.75     19242
+           1       0.78      0.63      0.70     19348
+'''
+score = cross_val_score(svc, X_train, y_train, cv=5, scoring='accuracy', n_jobs=-1, verbose=1)
+print("Input X_train --> f1: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
+# Input X_train --> f1: 0.727 (+/- 0.005)
+# %%
+# KNN:
+for k in range(5, 25, 1):
+    print('k = ', k)
+    neighbors = KNeighborsClassifier(n_neighbors=k, n_jobs=-1, weights='distance')
+    neighbors.fit(X_train, y_train)
+    y_pred = neighbors.predict(X_test)
+    #print('Confusion Matrix\n', pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predicted'], margins=True))
+    print('KNN:\n', classification_report(y_test, y_pred, target_names=['0', '1']))
+    #fpr, tpr, thresholds = roc_curve(y_test, y_pred, pos_label=1)
+    #print('AUC: ', auc(fpr, tpr))
+    # Cross Validation
+    #score = cross_val_score(neighbors, X_test, y_test, cv=5, scoring='accuracy', n_jobs=-1)
+    #print("KNN: Input X --> f1: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
+'''
+Without Under-Sampling:
+               precision    recall  f1-score   support
+k=19
+Under-Sampling
+           0       0.70      0.77      0.74     19242
+           1       0.75      0.67      0.71     19348
+'''
 #%% [markdown]
 # #### Final model evaluation:
-# The best model in all approaches is gradient boosting.
-# Approaches are:
-# * Without Under-Sampling --> 0.99/0.91 recall
-# * Under-Sampling --> 0.95/0.95 recall
-# * SelectKBest=100 --> 0.97/0.99 recall
-# Every new approach boost the model performance, with SelectKBest winning in this case. 
-# Other models could not cope with under-sampling or SelectKBest strategies as shown with gradient boosting.
-# Looks like boosting really helps to learn the more difficult cases as well. 
-# And that with no overfitting. The variance of the cross validation score is tiny.
+# The best model logistic regression with a f1-score of 0.87.
 
 #### Other models
-# * SVM cannot handle that much data. The performance is therefore quite poor.  
-# * KNN has a similar problem. It gets harder for KNN to process a lot of data.
-# * RandomForest is the second best model. Specially with SelectKBest --> 0.95/0.94 recall
-# * Decision tree could not improve much with under-sampling and got worse with SelectKBest
-# * Naive Bayes is really fast to compute and results are quite good: SelectKBest --> 0.91/0.89 recall
-# * Logistic Regression get very inefficient with a lot of data. The results are very good: Under-sampling --> 0.95/0.94 recall
+# * SVM cannot has decent results and is not fast to compute. 
+# * KNN has a similar problem. It gets harder for KNN to process a lot of data points.
+# * A single Decision tree is not working because vectorized data.
+# * RandomForest is better but with decent results. 
+# * Naive Bayes is really fast to compute but the results are bad.
+# * Gradient Boosting the second best model.
