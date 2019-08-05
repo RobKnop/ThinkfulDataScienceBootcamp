ca6743503b9e37039558c04ba6ce70afb441f77a
Robert Knop <robert.knop@nordakademie.org>
2019-08-05 22:48:24 +0200

    test KNN

--- Main/4.5_NLPCapstone.py
+++ Main/4.5_NLPCapstone.py
@@ -519,7 +519,7 @@ X = df_model.drop(columns=['category'])
 y = df_model['category'].astype(int)
 
 # Split into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
 
 #%%
 # Random Forest: 
@@ -544,11 +544,11 @@ grid_obj.best_estimator_
 
 #%%
 rfc = ensemble.RandomForestClassifier(bootstrap=True, class_weight=None, criterion='entropy',
-                       max_depth=13, max_features='log2', max_leaf_nodes=None,
+                       max_depth=10, max_features='log2', max_leaf_nodes=None,
                        min_impurity_decrease=0.0, min_impurity_split=None,
-                       min_samples_leaf=2, min_samples_split=2,
-                       min_weight_fraction_leaf=0.0, n_estimators=64, n_jobs=4,
-                       oob_score=False, random_state=None, verbose=0,
+                       min_samples_leaf=1, min_samples_split=3,
+                       min_weight_fraction_leaf=0.0, n_estimators=96, n_jobs=4,
+                       oob_score=False, random_state=42, verbose=0,
                        warm_start=False)
 
 # Fit the best algorithm to the data. 
@@ -563,8 +563,25 @@ score = cross_val_score(rfc, X, y, cv=5, n_jobs=-1, verbose=1)
 print("RFC: Input X --> Recall: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
 
 #%%
+for k in range(5, 25, 1):
+    print('k = ', k)
+    neighbors = KNeighborsClassifier(n_neighbors=k, n_jobs=-1, weights='distance')
+    neighbors.fit(X_train, y_train)
 
+    print('train: ', neighbors.score(X_train, y_train))
+    print('test: ', neighbors.score(X_test, y_test))
+#%%
+neighbors = KNeighborsClassifier(n_neighbors=8, n_jobs=-1, weights='distance')
+neighbors.fit(X_train, y_train)
 
+print('train: ', neighbors.score(X_train, y_train))
+print('test: ', neighbors.score(X_test, y_test))
+
+y_pred = neighbors.predict(X_test)
+print('Confusion Matrix\n', pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predicted'], margins=True))
+print('gbc:\n', classification_report(y_test, y_pred, target_names=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']))
+score = cross_val_score(neighbors, X, y, cv=5, n_jobs=-1, verbose=1)
+print("gbc: Input X --> Recall: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
 #%%
 
 
