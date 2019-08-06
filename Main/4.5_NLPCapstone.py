ca3e448e048e7dfe56299fa5b0513eb0f3dbcb20
Robert Knop <robert.knop@nordakademie.org>
2019-08-07 00:04:08 +0200

    clean the code, add comments

--- Main/4.5_NLPCapstone.py
+++ Main/4.5_NLPCapstone.py
@@ -1,3 +1,5 @@
+#%% [markdown]
+# # Capstone Project Natural Language Processing (Unsupervised and Supervised Learning)
 #%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
 # ms-python.python added
 import os
@@ -6,7 +8,7 @@ try:
 	print(os.getcwd())
 except:
 	pass
-#%%
+
 import nltk
 import pandas as pd
 import numpy as np
@@ -20,11 +22,11 @@ from mpl_toolkits.mplot3d import Axes3D
 import IPython
 from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import RegexpTokenizer
+from collections import Counter 
 from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.decomposition import TruncatedSVD
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
@@ -34,32 +36,40 @@ import gensim
 # Load models
 from sklearn import ensemble
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.cluster import KMeans
 
 from sklearn.utils import shuffle
-from sklearn.metrics import classification_report, confusion_matrix, roc_curve, auc, recall_score
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import cross_val_score, GridSearchCV, train_test_split
 from sklearn import metrics
-#%% 
-TODO: Bar graphs
 #%% [markdown]
-# 10 Guest and styles: 
+# ## Experiment Design
+# Podcasts getting more and more mainstream in recent years. A lot of people are producing really good content, most of the time for free.  
+# But what all this has to do with NLP? If you transcribe podcast you are converting the audio into a text respresentation. From there you are able to use the regularly NLP techniques. 
+# In this experiment we wil use the transcript from the Tim Ferris Show: https://tim.blog/2018/09/20/all-transcripts-from-the-tim-ferriss-show/  
+# ### Goal / Task
+# 1. Cluster 10 different podcast guests or styles. See if you find appropriate clusters. Create some graphs to support the analysis.
+# 2. Use Supervised modeling techniques to predict which podcast episode comes from which guest or style.  
+#
+# ### Data set
 # 
-# 0. Tim Ferriss himself (solo)
-# 1. RadnomShow (5x)
+# #### 10 different podcast guest or styles: 
+# 
+# 0. Tim Ferriss himself / solo (9x)
+# 1. RadnomShow (8x)
 # 2. Kevin Kelly (4x)
 # 3. Dom D’Agostino (3x)
-# 4. Tony Robins
-# 5. Ramit Sethi
-# 6. Waitzkin
-# 7. Sacca 
-# 8. Peter Attias
-# 9. Cal Fussman
+# 4. Tony Robins (4x)
+# 5. Ramit Sethi (4x)
+# 6. Waitzkin (4x)
+# 7. Sacca (3x)
+# 8. Peter Attias (3x)
+# 9. Cal Fussman (3x)
+# --> 45 episodes
 
 #%%
 # Import raw data
-# This data set contains the transcripts of Tim Ferriss Podcasts
+# This data set contains the transcripts of The Tim Ferriss Podcasts
 # Source: https://tim.blog/2018/09/20/all-transcripts-from-the-tim-ferriss-show/
 df = pd.read_csv("./data/tim-ferriss-podcast/thinkful.csv")
 df.columns=['U', 'id', 'title', 'text']
@@ -85,10 +95,11 @@ def standardize_text(df, text_field):
 df['title'] = df['title'].str.replace(r"The Tim Ferriss Show Transcripts: ", "")
 df['title'] = df['title'].str.replace(r"Transcripts: ", "")
 df['title'] = df['title'].str.replace(r"Tim Ferriss Show Transcript: ", "")
-
 df['title'] = df['title'].str.replace(r"Episode ", "")
+
 df = standardize_text(df, "text")
 #%%
+# Do standard NLP processing
 # Tokenize
 tokenizer = RegexpTokenizer(r'\w+')
 
@@ -116,16 +127,7 @@ print("%s words total, with a vocabulary size of %s" % (len(all_words), len(VOCA
 print("Max episode length is %s" % max(sentence_lengths))
 print("Mean episode length is %s" % np.mean(sentence_lengths))
 
-def plot_TSNE(test_data, test_labels, plot=True):
-        tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300, early_exaggeration=20)
-        tsne_results = tsne.fit_transform(test_data) #for TFIDF -> test_data.toarray()
-        #print(tsne_results)
-        if plot:
-            x = tsne_results[:,0]
-            y = tsne_results[:,1]
-            plt.scatter(x, y, s=10, alpha=1)
-            for i,label in enumerate(test_labels):
-                plt.text(x[i], y[i], label, fontsize=14)
+# Define plotting functions
 
 def plot_LSA(test_data, test_labels, plot=True):
         lsa = TruncatedSVD(n_components=2)
@@ -141,6 +143,7 @@ def plot_LSA(test_data, test_labels, plot=True):
 #%% [markdown]
 # ## TF DF
 #%%
+# Define X and the date point label
 X = df["text"].tolist()
 label = df["title"].tolist()     
 
@@ -154,13 +157,8 @@ def tfidf(data):
 X_tfidf, tfidf_vectorizer = tfidf(X)
 
 # Plot TFIDF
-
 fig = plt.figure(figsize=(50, 40))
 plot_LSA(X_tfidf, label)
-plt.show()
-
-fig = plt.figure(figsize=(50, 40))
-plot_TSNE(X_tfidf.toarray(), label)
 plt.show()
 
 #%%
@@ -186,58 +184,34 @@ def get_word2vec_embeddings(vectors, df, generate_missing=False):
     return list(embeddings)
 
 embeddings = get_word2vec_embeddings(word2vec, df)
-#%%
+
+# Plot Word2Vec
 fig = plt.figure(figsize=(50, 40))         
 plot_LSA(embeddings, label)
 plt.show()
 
-fig = plt.figure(figsize=(50, 40))
-plot_TSNE(embeddings, label)
-plt.show()
-
-#%% 
-## Clustering
+#%% [markdown]
+# ## Clustering (Unsupervised)
+#%%
 df_emb = pd.DataFrame(embeddings)
 
 # Calculate predicted values.
 km = KMeans(n_clusters=10, random_state=42).fit(df_emb)
 y_pred = km.predict(df_emb)
 
-print('silhouette score', metrics.silhouette_score(df_emb, y_pred, metric='euclidean'))
-
-# 2D
-tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300, early_exaggeration=20)
-tsne_results = tsne.fit_transform(df_emb.values)
-# Plot the solution.
-plt.scatter(tsne_results[:, 0], tsne_results[:, 1], c=y_pred)
-plt.show()
-
-# 3D
-tsne = TSNE(n_components=3, verbose=1, perplexity=40, n_iter=300, early_exaggeration=20)
-tsne_results = tsne.fit_transform(df_emb.values)
-tsne_results = pd.DataFrame(tsne_results)
-df_y = pd.DataFrame(y_pred, columns=['y_pred'])
-
-df_y['y_pred'] = df_y['y_pred'].astype(int)
-
-tsne_results = pd.concat([tsne_results, df_y], axis=1)
-
-# Plot the solution.
-fig = pyplot.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(tsne_results[0], tsne_results[1], tsne_results[2], c=tsne_results['y_pred'])
-pyplot.show()
+print('silhouette score: ', metrics.silhouette_score(df_emb, y_pred, metric='euclidean'))
 
 # 2D
 lsa = TruncatedSVD(n_components=2)
 las_results = lsa.fit_transform(df_emb.values)
 las_results = pd.DataFrame(las_results)
-df_y = pd.DataFrame(y_pred, columns=['y_pred'])
 
+df_y = pd.DataFrame(y_pred, columns=['y_pred'])
 df_y['y_pred'] = df_y['y_pred'].astype(int)
 
 las_results = pd.concat([las_results, df_y], axis=1)
-        
+
+#Plot        
 plt.scatter(las_results[0].values, las_results[1].values, c=y_pred)
 plt.show()
 
@@ -245,37 +219,34 @@ plt.show()
 lsa = TruncatedSVD(n_components=3)
 las_results = lsa.fit_transform(df_emb.values)
 las_results = pd.DataFrame(las_results)
-df_y = pd.DataFrame(y_pred, columns=['y_pred'])
 
+df_y = pd.DataFrame(y_pred, columns=['y_pred'])
 df_y['y_pred'] = df_y['y_pred'].astype(int)
 
 las_results = pd.concat([las_results, df_y], axis=1)
-# Plot the solution.
-from matplotlib import pyplot
-from mpl_toolkits.mplot3d import Axes3D
 
 fig = pyplot.figure()
 ax = fig.add_subplot(111, projection='3d')
-
 ax.scatter(las_results[0].values, las_results[1].values, las_results[2].values, c=las_results['y_pred'])
 pyplot.show()
 
-# combine to see results
+# Combine df with y_pred to see cluster results
 results = pd.concat([df, df_y], axis=1)
 results = results.drop(columns=['text'])
 #%%
-# Visualize
+# Visualize cluster result
+# Plot the most frequent words per cluster
 for i in range(0,9,1):
     counter = Counter([word for tokens in results[results["y_pred"] == i].tokens for word in tokens]) 
     pd.DataFrame(counter.most_common(12), columns=('most frequent words', 'count')).plot.bar('most frequent words',1, title='Cluster ' + str(i))
     plt.xticks(rotation=40)
     plt.show()
-
+#%% [markdown]
+# ### Findings from doing clustering:
+#%% [markdown]
+# ## Supervised Learning Modeling
 #%%
-# Supervised Learning Modeling
-
-## Get the categories
-
+# Get the categories (y)
 categories = pd.DataFrame(np.array([
     # Random Show
      [24,  1]
@@ -336,14 +307,14 @@ categories = pd.DataFrame(np.array([
     ,[240, 0]
     ]),
     columns=['id', 'category'])
-
+# Join with current dataframe "df"
 df_model = pd.merge(df, categories, how='inner', on='id')
 # Delete duplicated data point
 df_model = df_model[df_model['id'] != 25]
 df_model = df_model[df_model['id'] != 26]
 
+# Define X and y
 X = df_model['text']
-
 y = df_model['category']
 
 # Split into train and test sets
@@ -354,6 +325,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 X_train_tfidf, tfidf_vectorizer = tfidf(X_train)
 X_test_tfidf = tfidf_vectorizer.transform(X_test)
 
+# Try Random Forrest
 rfc = ensemble.RandomForestClassifier(criterion='entropy', n_jobs=4, n_estimators=64)
 # Fit the best algorithm to the data. 
 rfc.fit(X_train_tfidf, y_train)
@@ -367,8 +339,8 @@ print('RFC:\n', classification_report(y_test, y_pred, target_names=['0', '1', '2
 # Use word2vec embeddings
 df_model = pd.concat([df_emb, categories], axis=1)
 
+# Define X and y
 X = df_model.drop(columns=['category'])
-
 y = df_model['category']
 
 # Split into train and test sets
@@ -393,7 +365,7 @@ grid_obj = GridSearchCV(rfc, parameters, cv=3, n_jobs=-1, verbose=1)
 grid_obj.fit(X, y)
 
 # Set the clf to the best combination of parameters
-rfc = grid_obj.best_estimator_
+grid_obj.best_estimator_
 
 #%%
 rfc = ensemble.RandomForestClassifier(bootstrap=True, class_weight=None, criterion='entropy',
@@ -415,9 +387,8 @@ print('RFC:\n', classification_report(y_test, y_pred, target_names=['0', '1', '2
 score = cross_val_score(rfc, X, y, cv=5, n_jobs=-1, verbose=1)
 print("RFC: Input X --> Recall: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
 
-#%%
-df_model = pd.merge(df, categories, how='inner', on='id')
-df_model = df_model.drop(columns=['tokens'])
+#%% [markdown]
+# ### Augment data
 
 #%%
 df_tmp = pd.DataFrame(columns=('id', 'title', 'text', 'category'))
@@ -441,8 +412,8 @@ for index, row in df_model.iterrows():
 
 
 #%%
+# Define X and y
 X = df_tmp['text']
-
 y = df_tmp['category'].astype('int')
 
 # Split into train and test sets
@@ -471,7 +442,7 @@ grid_obj = GridSearchCV(rfc, parameters, cv=3, n_jobs=-1, verbose=1)
 grid_obj.fit(X_train_tfidf, y_train)
 
 # Set the clf to the best combination of parameters
-rfc = grid_obj.best_estimator_
+grid_obj.best_estimator_
 
 #%%
 rfc = ensemble.RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
@@ -519,9 +490,8 @@ df_emb.reset_index(drop=True, inplace=True)
 df_tmp.reset_index(drop=True, inplace=True)
 df_model = pd.concat([df_emb, df_tmp['category']], axis=1)
 
-
+# Define X and y
 X = df_model.drop(columns=['category'])
-
 y = df_model['category'].astype(int)
 
 # Split into train and test sets
@@ -550,11 +520,11 @@ grid_obj.best_estimator_
 
 #%%
 rfc = ensemble.RandomForestClassifier(bootstrap=True, class_weight=None, criterion='entropy',
-                       max_depth=4, max_features='auto', max_leaf_nodes=None,
+                       max_depth=10, max_features='log2', max_leaf_nodes=None,
                        min_impurity_decrease=0.0, min_impurity_split=None,
-                       min_samples_leaf=1, min_samples_split=2,
-                       min_weight_fraction_leaf=0.0, n_estimators=80, n_jobs=4,
-                       oob_score=False, random_state=22, verbose=0,
+                       min_samples_leaf=2, min_samples_split=3,
+                       min_weight_fraction_leaf=0.0, n_estimators=96, n_jobs=4,
+                       oob_score=False, random_state=None, verbose=0,
                        warm_start=False)
 
 # Fit the best algorithm to the data. 
