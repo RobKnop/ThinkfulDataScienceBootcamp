6accfb7812179f2bac5a0554dd50ac6e9c1c0765
Robert Knop <robert.knop@nordakademie.org>
2019-08-09 12:12:34 +0200

    reimort ipynb to have the current python file

--- Main/4.5_NLPCapstone.py
+++ Main/4.5_NLPCapstone.py
@@ -1,6 +1,13 @@
+# To add a new cell, type '#%%'
+# To add a new markdown cell, type '#%% [markdown]'
+#%%
+from IPython import get_ipython
+
 #%% [markdown]
-# # Capstone Project Natural Language Processing (Unsupervised and Supervised Learning)
-#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+#  # Capstone Project Natural Language Processing 
+#  ## (Unsupervised and Supervised Learning)
+
+#%%
 # ms-python.python added
 import os
 try:
@@ -19,6 +26,7 @@ import codecs
 import matplotlib.pyplot as plt
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
 import IPython
 from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -42,30 +50,31 @@ from sklearn.utils import shuffle
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import cross_val_score, GridSearchCV, train_test_split
 from sklearn import metrics
+
 #%% [markdown]
-# ## Experiment Design
-# Podcasts getting more and more mainstream in recent years. A lot of people are producing really good content, most of the time for free.  
-# But what all this has to do with NLP? If you transcribe podcast you are converting the audio into a text respresentation. From there you are able to use the regularly NLP techniques. 
-# In this experiment we wil use the transcript from the Tim Ferris Show: https://tim.blog/2018/09/20/all-transcripts-from-the-tim-ferriss-show/  
-# ### Goal / Task
-# 1. Cluster 10 different podcast guests or styles. See if you find appropriate clusters. Create some graphs to support the analysis.
-# 2. Use Supervised modeling techniques to predict which podcast episode comes from which guest or style.  
-#
+#  ## Experiment Design
+#  Podcasts getting more and more mainstream in recent years. A lot of people are producing really good content, most of the time for free.
+#  But what all this has to do with NLP? If you transcribe podcast you are converting the audio into a text respresentation. From there you are able to use the regularly NLP techniques.
+#  In this experiment we wil use the transcript from the Tim Ferris Show: https://tim.blog/2018/09/20/all-transcripts-from-the-tim-ferriss-show/
+#  ### Goal / Task
+#  1. Cluster 10 different podcast guests or styles. See if you find appropriate clusters. Create some graphs to support the analysis.
+#  2. Use Supervised modeling techniques to predict which podcast episode comes from which guest or style.
+# 
 # ### Data set
 # 
-# #### 10 different podcast guest or styles: 
+# #### 10 different podcast guest or styles:
 # 
-# 0. Tim Ferriss himself / solo (9x)
-# 1. RadnomShow (8x)
-# 2. Kevin Kelly (4x)
-# 3. Dom D’Agostino (3x)
-# 4. Tony Robins (4x)
-# 5. Ramit Sethi (4x)
-# 6. Waitzkin (4x)
-# 7. Sacca (3x)
-# 8. Peter Attias (3x)
-# 9. Cal Fussman (3x)
-# --> 45 episodes
+#  0. Tim Ferriss himself / solo (9x)
+#  1. RadnomShow (8x)
+#  2. Kevin Kelly (4x)
+#  3. Dom D’Agostino (3x)
+#  4. Tony Robins (4x)
+#  5. Ramit Sethi (4x)
+#  6. Waitzkin (4x)
+#  7. Sacca (3x)
+#  8. Peter Attias (3x)
+#  9. Cal Fussman (3x)
+#  --> 45 episodes
 
 #%%
 # Import raw data
@@ -75,6 +84,8 @@ df = pd.read_csv("./data/tim-ferriss-podcast/thinkful.csv")
 df.columns=['U', 'id', 'title', 'text']
 df = df.drop(columns=['U'])
 df.head()
+
+
 #%%
 def standardize_text(df, text_field):
     df[text_field] = df[text_field].str.replace(r"http\S+", "")
@@ -98,6 +109,8 @@ df['title'] = df['title'].str.replace(r"Tim Ferriss Show Transcript: ", "")
 df['title'] = df['title'].str.replace(r"Episode ", "")
 
 df = standardize_text(df, "text")
+
+
 #%%
 # Do standard NLP processing
 # Tokenize
@@ -141,7 +154,8 @@ def plot_LSA(test_data, test_labels, plot=True):
                 plt.text(x[i], y[i], label, fontsize=14)
 
 #%% [markdown]
-# ## TF DF
+#  ## TF-IDF
+
 #%%
 # Define X and the date point label
 X = df["text"].tolist()
@@ -160,6 +174,7 @@ X_tfidf, tfidf_vectorizer = tfidf(X)
 fig = plt.figure(figsize=(50, 40))
 plot_LSA(X_tfidf, label)
 plt.show()
+
 
 #%%
 # Word2Vec
@@ -191,7 +206,8 @@ plot_LSA(embeddings, label)
 plt.show()
 
 #%% [markdown]
-# ## Clustering (Unsupervised)
+#  ## Clustering (Unsupervised)
+
 #%%
 df_emb = pd.DataFrame(embeddings)
 
@@ -204,16 +220,18 @@ print('silhouette score: ', metrics.silhouette_score(df_emb, y_pred, metric='euc
 # 2D
 lsa = TruncatedSVD(n_components=2)
 las_results = lsa.fit_transform(df_emb.values)
-las_results = pd.DataFrame(las_results)
+las_results = pd.DataFrame(las_results, columns=['x', 'y'])
 
 df_y = pd.DataFrame(y_pred, columns=['y_pred'])
 df_y['y_pred'] = df_y['y_pred'].astype(int)
 
 las_results = pd.concat([las_results, df_y], axis=1)
 
-#Plot        
-plt.scatter(las_results[0].values, las_results[1].values, c=y_pred)
-plt.show()
+#Plot
+lm = sns.lmplot(data=las_results, x='x', y='y', hue='y_pred',
+                   fit_reg=False, legend=True, legend_out=True)
+fig = lm.fig
+fig.suptitle("KMeans Cluster - Fig. 1", fontsize=12)
 
 # 3D
 lsa = TruncatedSVD(n_components=3)
@@ -228,24 +246,38 @@ las_results = pd.concat([las_results, df_y], axis=1)
 fig = pyplot.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(las_results[0].values, las_results[1].values, las_results[2].values, c=las_results['y_pred'])
+ax.set_title("KMeans Cluster 3D - Fig. 2")
 pyplot.show()
 
 # Combine df with y_pred to see cluster results
 results = pd.concat([df, df_y], axis=1)
 results = results.drop(columns=['text'])
+
+
 #%%
 # Visualize cluster result
 # Plot the most frequent words per cluster
-for i in range(0,9,1):
+for i in range(0,10,1):
     counter = Counter([word for tokens in results[results["y_pred"] == i].tokens for word in tokens]) 
     pd.DataFrame(counter.most_common(12), columns=('most frequent words', 'count')).plot.bar('most frequent words',1, title='Cluster ' + str(i))
     plt.xticks(rotation=40)
     plt.show()
+
+
+#%%
+results.drop(columns=['tokens']).sort_values('y_pred')
+
 #%% [markdown]
-# ### Findings from doing clustering:
-# 
+#  ### Findings from doing clustering:
+# The first four clusters are quite close to each other. The most frequent words are almost the same for each cluster.  If you take a look on the 2D plot (Fig. 1) you can see that specily the cluster __0__ and __3__ is really close to each other. So this is supporting the first finding.    
+# __Cluster 4__ has quite different frequent words (like grateful, feel) and showing that in this cluster the topic is way different than from the other clusters.  
+# __Cluster 6__ correctly identified every podcast with Cal Fussman.  
+# __Cluster 7__ also correctly spotted a special guest (Dominic) who is an expert in ketogenic diet. The frequent words are supporting this finding. "Diet", "Ketogenic" are one of the most frequent used words.  
+# __Cluster 8__ is similar to cluster 0 till 3.  
+# __Cluster 9__ is separated well. Also the most frequently used words are unique to compared to each other cluster.
 #%% [markdown]
-# ## Supervised Learning Modeling
+#  ## Supervised Learning Modeling
+
 #%%
 # Get the categories (y)
 categories = pd.DataFrame(np.array([
@@ -316,6 +348,7 @@ y = df_model['category']
 # Split into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=90)
 
+
 #%%
 # Use TFIDF
 X_train_tfidf, tfidf_vectorizer = tfidf(X_train)
@@ -327,6 +360,8 @@ rfc = ensemble.RandomForestClassifier(criterion='entropy', n_jobs=4, n_estimator
 rfc.fit(X_train_tfidf, y_train)
 print('train: ', rfc.score(X_train_tfidf, y_train))
 print('test: ', rfc.score(X_test_tfidf, y_test))
+
+
 #%%
 # Use word2vec embeddings
 df_word2vec = pd.concat([df_emb, categories], axis=1)
@@ -337,6 +372,7 @@ y = df_word2vec['category']
 
 # Split into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20)
+
 
 #%%
 # Random Forest: 
@@ -359,12 +395,13 @@ grid_obj.fit(X, y)
 # Set the clf to the best combination of parameters
 grid_obj.best_estimator_
 
+
 #%%
 rfc = ensemble.RandomForestClassifier(bootstrap=True, class_weight=None, criterion='entropy',
-                       max_depth=13, max_features='log2', max_leaf_nodes=None,
+                       max_depth=5, max_features='auto', max_leaf_nodes=None,
                        min_impurity_decrease=0.0, min_impurity_split=None,
-                       min_samples_leaf=2, min_samples_split=2,
-                       min_weight_fraction_leaf=0.0, n_estimators=64, n_jobs=4,
+                       min_samples_leaf=1, min_samples_split=3,
+                       min_weight_fraction_leaf=0.0, n_estimators=32, n_jobs=4,
                        oob_score=False, random_state=None, verbose=0,
                        warm_start=False)
 
@@ -380,7 +417,12 @@ score = cross_val_score(rfc, X, y, cv=5, n_jobs=-1, verbose=1)
 print("RFC: Input X --> Recall: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
 
 #%% [markdown]
+# ### Findings:
+# Starting of with TF-IDF and RandomForrest shows a poor performance. 
+# With Word2Vec the performance increased significantly to 0.7. This is still poor and as the error message is showing that we even had not predicted every possible category. To fix the low performance and this error, let's create more data points. 
+#%% [markdown]
 # ### Augment data
+# We are going to split every podcast episode text into 5 equal sized parts. This is how we augment our data set with more text pieces we can learn and predict from. 
 
 #%%
 df_tmp = pd.DataFrame(columns=('id', 'title', 'text', 'category'))
@@ -409,13 +451,14 @@ y = df_tmp['category'].astype('int')
 # Split into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20)
 
+
 #%%
 # Use TFIDF
 X_train_tfidf, tfidf_vectorizer = tfidf(X_train)
 X_test_tfidf = tfidf_vectorizer.transform(X_test)
 
 # Random Forest: 
-rfc = ensemble.RandomForestClassifier(n_jobs=4)
+rfc = ensemble.RandomForestClassifier(n_jobs=4, random_state=44)
 
 # Choose some parameter combinations to try
 parameters = {
@@ -434,13 +477,14 @@ grid_obj.fit(X_train_tfidf, y_train)
 # Set the clf to the best combination of parameters
 grid_obj.best_estimator_
 
+
 #%%
 rfc = ensemble.RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
                        max_depth=13, max_features='sqrt', max_leaf_nodes=None,
                        min_impurity_decrease=0.0, min_impurity_split=None,
                        min_samples_leaf=2, min_samples_split=5,
                        min_weight_fraction_leaf=0.0, n_estimators=96, n_jobs=4,
-                       oob_score=False, random_state=None, verbose=0,
+                       oob_score=False, random_state=44, verbose=0,
                        warm_start=False)
 
 # Fit the best algorithm to the data. 
@@ -451,6 +495,13 @@ print('test: ', rfc.score(X_test_tfidf, y_test))
 y_pred = rfc.predict(X_test_tfidf)
 print('Confusion Matrix\n', pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predicted'], margins=True))
 print('RFC:\n', classification_report(y_test, y_pred, target_names=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']))
+score = cross_val_score(rfc, X_train_tfidf, y_train, cv=5, n_jobs=-1, verbose=1)
+print("RFC: Input X --> Recall: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
+
+#%% [markdown]
+# ### Results
+# TF-IDF is working much better and has a great performance now. F1-score is much higher than the performance without data augmentation. Cross validation shows this one is a highly overfitted model.  
+# Let's try Word2Vec embeddings to find out if we can improve from here.
 
 #%%
 # Tokenize
@@ -471,6 +522,7 @@ df_tmp["tokens"] = df_tmp["tokens"].map(lambda x: [w for w in x if not w == 'fer
 lemmer = WordNetLemmatizer()
 df_tmp["tokens"] = df_tmp["tokens"].map(lambda x: [lemmer.lemmatize(w) for w in x])
 
+
 #%%
 embeddings = get_word2vec_embeddings(word2vec, df_tmp)
 df_emb = pd.DataFrame(embeddings)
@@ -486,6 +538,7 @@ y = df_word2vec['category'].astype(int)
 
 # Split into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
+
 
 #%%
 # Random Forest: 
@@ -508,6 +561,7 @@ grid_obj.fit(X, y)
 # Set the clf to the best combination of parameters
 grid_obj.best_estimator_
 
+
 #%%
 rfc = ensemble.RandomForestClassifier(bootstrap=True, class_weight=None, criterion='entropy',
 max_depth=6, max_features='auto', max_leaf_nodes=None,
@@ -528,6 +582,7 @@ print('RFC:\n', classification_report(y_test, y_pred, target_names=['0', '1', '2
 score = cross_val_score(rfc, X, y, cv=5, n_jobs=-1, verbose=1)
 print("RFC: Input X --> Recall: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
 
+
 #%%
 for k in range(5, 25, 1):
     print('k = ', k)
@@ -536,6 +591,8 @@ for k in range(5, 25, 1):
 
     print('train: ', neighbors.score(X_train, y_train))
     print('test: ', neighbors.score(X_test, y_test))
+
+
 #%%
 neighbors = KNeighborsClassifier(n_neighbors=8, n_jobs=-1, weights='distance')
 neighbors.fit(X_train, y_train)
@@ -548,8 +605,13 @@ print('Confusion Matrix\n', pd.crosstab(y_test, y_pred, rownames=['True'], colna
 print('gbc:\n', classification_report(y_test, y_pred, target_names=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']))
 score = cross_val_score(neighbors, X, y, cv=5, n_jobs=-1, verbose=1)
 print("gbc: Input X --> Recall: %0.3f (+/- %0.3f)" % (score.mean(), score.std() * 2))
-#%% [markdown]
-# ### Findings:
 
 #%% [markdown]
-# ### Conclusion:
+#  ### Findings:
+#  KNN has the worst performance (F1-score), even it overfits less. 
+#  Word2Vec embeddings with Random Forest is the winning model here. 
+#  There is still some overfitting but not much than TF-IDF. 
+#%% [markdown]
+#  ### Conclusion:
+#  Augmentation is a huge improvement and Word2Vec embeddings work the best. 
+
